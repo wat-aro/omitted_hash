3ff5e03564c73e3300d08e8dbb5c9496951bb087
wat-aro <kazutas1008@gmail.com>
2021-12-22 00:23:08 +0900

    Add article model

---
+++ db/migrate/20211221151954_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title, null: false
+      t.text :body, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/articles.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :article do
+    user { nil }
+    title { "たいとる" }
+    body { "ほんぶん" }
+
+    trait :with_user do
+      user { create(:user) }
+    end
+  end
+end
